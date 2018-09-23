1451aa5b50582b4b944a977528ee2c48cdee230d
Katooosky MacbookPro <katoosky5230.enjoy.dev@gmail.com>
2018-09-23 23:15:24 +0900

    Add Threading with coroutine.

--- bot_manager.py
+++ bot_manager.py
@@ -1,6 +1,12 @@
+import asyncio
 from threading import Thread
 
 import bot1
 
+loop = asyncio.get_event_loop()
 # bot1の起動
-Thread(target=bot1.bot.run, args=(bot1.token,)).start()
+job = Thread(target=asyncio.run_coroutine_threadsafe, args=(bot1.bot.start(bot1.token, Bot=True), loop))
+job.start()
+
+
+loop.run_forever()
