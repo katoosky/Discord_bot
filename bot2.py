3c7a81d4576263b3111e73ddaec5ff10204f5364
Katooosky MacbookPro <katoosky5230.enjoy.dev@gmail.com>
2018-09-27 02:20:08 +0900

    Update version.

--- bot2.py
+++ bot2.py
@@ -7,7 +7,7 @@ import psycopg2
 from psycopg2.extras import DictCursor
 
 # 変数
-version="1.1.9"
+version="1.1.10"
 token = "NDkzOTI2MDI4NjIwODU3MzY0.DosFJA.1Hzepp-iPyU-MFk__HZ9-JKsY8g"
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("&"),
                    description='This is Botくん2号.')
