590579f3b6b2c3ec193ae06326a4930e49b3d91f
Katooosky MacbookPro <katoosky5230.enjoy.dev@gmail.com>
2018-09-28 07:22:18 +0900

    Bugfix.

--- bot2.py
+++ bot2.py
@@ -55,7 +55,7 @@ Botくん1号 Commands
         '瞳の色': 'eye_color',
         '体型': 'body',
         '性格': 'personality',
-        '口癖': 'catch_phrase,
+        '口癖': 'catch_phrase',
         '好きなもの': 'favorite',
         '嫌いなもの': 'dislike',
         '将来の夢': 'dream',
@@ -284,7 +284,7 @@ def help_mention():
     embed.add_field(name="三題噺", value="三題噺関連は*「@Botくん2号 三題噺」*から始まるよ！", inline=False)
     embed.add_field(name="お絵かき", value="お絵かき関連は*「@Botくん2号 お絵かき」*から始まるよ！\nでも、まだ未実装なんだ......ごめんね？", inline=False)
     add_help_three_topics(embed)
-    # add_help_drawing(embed)
+    add_help_drawing(embed)
     embed.add_field(name="Version", value=version)
     # give info about you here
     embed.add_field(name="Author", value="雅猫")
